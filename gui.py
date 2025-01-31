d06f642b3f43f00e0ac6bc8ee4f1681d86da7985
TabassumNova <tabassumnova1@gmail.com>
2025-01-31 10:53:50 +0100

    updated

--- gui.py
+++ gui.py
@@ -9,43 +9,84 @@ class Window(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.filenames = None
+        self.batchname = None
         self.raw_df = None
+        self.library_df = None
         self.setFixedWidth(400)
-        self.setFixedHeight(200)
+        self.setFixedHeight(400)
 
+        # For loading files
         self.btn1 = QPushButton(self)
         self.btn1.setObjectName('Load files')
         self.btn1.setText('Load files')
         self.btn1.setGeometry(QRect(10, 35, 111, 50))
         self.btn1.clicked.connect(self.open_file_dialog)
 
+        # Message for loading operation
         self.label1 = QLabel(self)
-        self.label1.setObjectName('Loaded')
+        self.label1.setObjectName('Data loaded')
         self.label1.setText('')
         self.label1.setStyleSheet("border: 0.5px solid gray;")
         self.label1.setGeometry(QRect(125, 40, 111, 40))
 
+        # For loading files
         self.btn2 = QPushButton(self)
-        self.btn2.setObjectName('Open Data')
-        self.btn2.setText('Open Data')
+        self.btn2.setObjectName('Load library')
+        self.btn2.setText('Load library')
         self.btn2.setGeometry(QRect(10, 100, 111, 50))
-        self.btn2.clicked.connect(self.open_images)
+        self.btn2.clicked.connect(self.open_library) # change this to read excel file
+
+        # Message for loading operation
+        self.label2 = QLabel(self)
+        self.label2.setObjectName('Library loaded')
+        self.label2.setText('')
+        self.label2.setStyleSheet("border: 0.5px solid gray;")
+        self.label2.setGeometry(QRect(125, 105, 111, 40))
+
+        # For opening data
+        self.btn3 = QPushButton(self)
+        self.btn3.setObjectName('Open Data')
+        self.btn3.setText('Open Data')
+        self.btn3.setGeometry(QRect(10, 165, 111, 50))
+        self.btn3.clicked.connect(self.open_data)
         
 
     def open_file_dialog(self):
         # Open the file dialog and get the selected file name
         self.filenames, _ = QFileDialog.getOpenFileNames(self)
         if self.filenames:
-            self.label1.setText("Loaded")
+            self.label1.setText("Data loaded")
             dataloader = Dataloader(self.filenames)
             self.raw_df = dataloader.raw_df
+        
+        # Get the batchname
+        clock = 2
+        file_path = self.filenames[0]
+        while clock:
+            file_path, folder = os.path.split(file_path)
+            clock -= 1
+        self.batchname = folder
+
+
+    def open_library(self):
+        # Open the file dialog and get the selected excel file
+        # Load the file as pandas dataframe
+        self.lib_path, _ = QFileDialog.getOpenFileName(self)
+        if self.filenames:
+            self.label2.setText("Library loaded")
+            self.library_df = pd.read_excel(self.lib_path)
+        pass
             
 
-    def open_images(self):
-        viz(self.raw_df)
+    def open_data(self):
+        # This creates vizualization to plot multiple data
+        viz(self.batchname, self.raw_df, self.library_df)
+        # Check if del obj is possible
         self.filenames = None
         self.raw_df = None
+        self.library_df = None
         self.label1.setText("")
+        self.label2.setText("")
 
 
 
--- vizualization.py
+++ vizualization.py
@@ -1,6 +1,8 @@
+import ast
 import plotly.graph_objects as go
+import plotly.express as px
 
-def viz(df):
+def viz(batch_name, df, library, sensor='SWIR'):
     wavelengths = list(df.columns)[1:]
     data = []
     for index, row in df.iterrows():
@@ -9,6 +11,33 @@ def viz(df):
         graph = go.Scatter(x=wavelengths, y=energy, name = sample_name, mode='lines')
         data.append(graph)
     fig = go.Figure(data=data)
+    # Select specific library according to sensor
+    # Iterate over the rows in the library and
+    # draw a vertical line at each wavelength
+    # Name them as polymer groups
+    # append the lines in data
+    line_groups = {}
+    lib = library[library['sensor'] == sensor]
+    # Generate a color map
+    unique_groups = lib['polymer'].unique()
+    color_map = {group: px.colors.qualitative.Plotly[i % len(px.colors.qualitative.Plotly)] for i, group in enumerate(unique_groups)}
+    for index, row in lib.iterrows():
+        group_name = row['polymer']
+        wavelengths = [float(x) for x in ast.literal_eval(row['wavelengths'])]
+        for i, x_value in enumerate(wavelengths):
+            fig.add_vline(
+                x=x_value, 
+                # line_dash="dash", 
+                line_color=color_map[group_name], 
+                opacity=0.5, 
+                name=group_name,
+                legendgroup=group_name,
+                showlegend = True
+                # showlegend=True if i == 0 else False
+            )
+        # line = go.Scatter(x=wavelengths, y=[1] * len(wavelengths), mode='lines', name=group_name)
+        # data.append(line)
+    # fig = go.Figure(data=data)
     fig.update_layout(
         xaxis_title='Wavelength',
         yaxis_title='Reflectance'
@@ -16,7 +45,7 @@ def viz(df):
     fig.update_layout(hovermode="x unified")
     # TODO
     # change the name according to the table name
-    fig.write_html("T16.html")
+    fig.write_html(batch_name + ".html")
      
     fig.show()
     
